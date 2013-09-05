dbd38208e82292034d5434a005b4514ef2016aae
duncky <d@duncky.com>
2013-09-05 19:02:31 +0100

    init

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-karma');
+
+
+  grunt.initConfig({
+    watch: {
+      files:['src/*.coffee'],
+      tasks:['coffee']
+    },
+    coffee: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: '*.coffee',
+          dest: '.tmp',
+          ext: '.js'
+        }]
+      },
+    },
+    karma: {
+        unit: {
+            configFile: 'karma.conf.js',
+            autoWatch: true
+        }
+    },
+    clean: {
+        dist: '.tmp'
+    }
+  });
+};
